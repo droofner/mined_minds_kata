f68916c6594009d8cf025d8fd408b72557fc5b3b
roofner <steelerbaby210@hotmail.com>
2016-08-02 13:30:27 -0400

    print_to_sceen

--- printing_to_sreen.rb
+++ printing_to_sreen.rb
@@ -1,5 +1,14 @@
 number = 1
 100.times do
-	if number = 3 puts "mined"
+	if  number %3 == 0 && number %5 == 0
+	 puts "minedminds"
+	elsif
+	number %5 == 0
+	puts "minds"
+elsif number %3 == 0 
+	puts "mined"
+
+else  puts number
+	end
 	number = number + 1
 end
