47250eaa7e8fa2c581c01db296c3606eb127614d
roofner <steelerbaby210@hotmail.com>
2016-08-07 23:33:07 -0400

    .\mined_minds_number.rb

--- mined_minds_number.rb
+++ mined_minds_number.rb
@@ -1,4 +1,13 @@
-
 def mined_minds(number)
-	number
+	if number % 3 == 0 && number % 5 == 0
+		puts "minedminds"
+	elsif number % 5 == 0
+		puts "minds"
+	elsif  number % 3 == 0
+		puts "mined"
+	else number
+		puts number = number + 1
+	end		
 end
+
+
--- mined_minds_test.rb
+++ mined_minds_test.rb
@@ -10,4 +10,17 @@ class Test_mined_minds < Minitest::Test
   def test_2_returns_2
   	assert_equal(2,mined_minds(2))
   end
+
+  def test_3_returns_mined
+  	assert_equal("mined", mined_minds(3))
+  end
+
+  def test_4_returns_minds
+  	assert_equal("minds", mined_minds(5))
+  end
+
+  def test_5_returns_mined_minds
+  	assert_equal("minedminds", mined_minds(15))
+  end
 end
+
